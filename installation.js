a1369a0c9eb76db3f553d93d73862086f2e35284
Derick Ostrenko <dostrenko@gmail.com>
2014-06-27 09:56:29 -0500

    Reversing Comm Direction

--- installation.js
+++ installation.js
@@ -1,14 +1,9 @@
-var serverPort = 3012;
+var io = require('socket.io-client');
 
-var ioc = require('socket.io-client');
-var client = ioc.connect("http://localhost:" + serverPort);
-
-client.once("connect", function() {
-	console.log('Client: Connected to port ' + serverPort);
-
-	client.emit("echo", "Hello World", function(message) {
-		console.log('Echo received: ', message);
-		// client.disconnect();
-		//server.close();
+var socket = io('http://localhost:3012');
+socket.on('news', function(data) {
+	console.log(data);
+	socket.emit('my other event', {
+		my: 'data'
 	});
 });
--- installationServerTest.js
+++ installationServerTest.js
@@ -1,14 +1,16 @@
 var port = 3012;
 
 var server = require('http').createServer().listen(port, function() {
-	console.log("Express server listening on port " + port);
+	console.log("server listening on port " + port);
 });
 
-var io = require('socket.io').listen(server).set("log level", 0);
+var io = require('socket.io').listen(server);
 
-io.sockets.on("connection", function(socket) {
-	console.log('Server: Incoming connection.');
-	socket.on("echo", function(msg, callback) {
-		callback(msg);
+io.on('connection', function(socket) {
+	socket.emit('news', {
+		hello: 'world'
+	});
+	socket.on('my other event', function(data) {
+		console.log(data);
 	});
 });
